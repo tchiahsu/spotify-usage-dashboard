005fc902d3f64b207fd584c728c236cac730dfe1
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-26 15:03:54 -0500

    setup routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/profile";
+import Artist from "./pages/artist";
+import Track from "./pages/track";
+import Recent from "./pages/recent";
+import Playlist from "./pages/playlist";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Profile />}  />
+          <Route path="/top-artists" element={<Artist />} />
+          <Route path="/top-tracks" element={<Track />} />
+          <Route path="/recently-played" element={<Recent />} />
+          <Route path="/playlists" element={<Playlist />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
