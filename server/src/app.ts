9b052ea2e2e2220d224265c05b81481e69f87f4a
Tony Hsu Tai <hsutai.c@gmail.com>
2026-01-03 16:21:15 -0500

    removed cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import authRouter from "./auth.js";
 import router from "./endpoints/routes.js";
 
 export const app = express();
 
-const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN ?? "https://spotifylisten.vercel.app/api";
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: FRONTEND_ORIGIN,
-    credentials: true,
-  })
-);
 
 app.use("/auth", authRouter);
 app.use("/", router);
